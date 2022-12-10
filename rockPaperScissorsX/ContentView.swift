8fe9fbe6c39c8fa00f4ba13228ee35dd4ff2d709
thaddeus0605 <tdronski@gmail.com>
2022-12-09 20:09:12 -0600

    Initial Basic Interface Setup, next step is logic

--- rockPaperScissorsX/ContentView.swift
+++ rockPaperScissorsX/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rps = ["✊🏻", "📄", "✂️"]
+    @State private var winOrLose = ["Win", "Lose"]
+    @State private var randomNumRPS = Int.random(in: 0...2)
+    @State private var randomNumWorL = Int.random(in: 0...1)
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Text("The computer wants you to: ")
+                    .font(.title)
+                Text(winOrLose[randomNumWorL])
+                    .font(.title.weight(.semibold))
+                Text("Against:")
+                    .font(.title)
+                Text(rps[randomNumRPS])
+                    .font(.largeTitle.weight(.semibold))
+                VStack(spacing: 15) {
+                    VStack{
+                        Text("Make your choice: ")
+                    }
+                    HStack{
+                        ForEach(0..<3) { number in
+                            Button {
+                                //Selection tapped
+                                print("Player selection tapped")
+                            } label: {
+                                Text(rps[number])
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
