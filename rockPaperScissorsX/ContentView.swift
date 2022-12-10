37bcea4815e55384fa62e3c2ce6a150c1bc30de2
thaddeus0605 <tdronski@gmail.com>
2022-12-09 20:57:03 -0600

    Completed game functionality

--- rockPaperScissorsX/ContentView.swift
+++ rockPaperScissorsX/ContentView.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var rps = ["✊🏻", "📄", "✂️"]
+    @State private var moveOptions = ["✊🏻", "📄", "✂️"]
     
-    @State private var randomNumRPS = Int.random(in: 0..<2)
+    @State private var computerSelection = Int.random(in: 0..<2)
     @State private var shouldWin = Bool.random()
+    
+    @State private var playerScore = 0
+    @State private var quesitonCount = 1
+    @State private var showingResults = false
    
-
-    
-    
     var body: some View {
         VStack {
             Spacer()
-            Text("Computer has selected...")
+            
+            Text("Computer has played...")
                 .font(.headline)
-            Text(rps[randomNumRPS])
+            Text(moveOptions[computerSelection])
                 .font(.system(size: 200))
             
             if shouldWin {
@@ -32,33 +34,60 @@ struct ContentView: View {
             } else {
                 Text("Which one loses?")
                     .foregroundColor(.red)
+                    .font(.title)
             }
             HStack {
                 ForEach(0..<3) { number in
-                    Button(rps[number]) {
-                        //Move function
-                        playGame(userChoice: number)
+                    Button(moveOptions[number]) {
+                        //play func
+                        playGame(choice: number)
                     }
                     .font(.system(size: 80))
-                    
                 }
             }
             Spacer()
-            Text("Score: ???")
-                .font(.headline)
+            Text("Player Score: \(playerScore)")
+                .font(.subheadline)
             Spacer()
         }
+        .alert("Game Over", isPresented: $showingResults) {
+            Button("Play Again", action: resetGame)
+        } message: {
+            Text("Your score is \(playerScore)")
+        }
     }
-    func playGame(userChoice: Int) {
+    
+    func playGame(choice: Int) {
         let winningMoves = [1, 2, 0]
         let didWin: Bool
         
         if shouldWin {
-            didWin = userChoice == winningMoves[randomNumRPS]
+            didWin = choice == winningMoves[computerSelection]
         } else {
-            didWin = winningMoves[userChoice] == randomNumRPS
+            // we lost
+            didWin = winningMoves[choice] == computerSelection
         }
         
+        if didWin {
+            playerScore += 1
+        } else {
+            playerScore -= 1
+        }
+        
+        if quesitonCount == 10 {
+            showingResults = true
+        } else {
+            computerSelection = Int.random(in: 0..<3)
+            shouldWin.toggle()
+            quesitonCount += 1
+        }
+    }
+    
+    func resetGame() {
+        computerSelection = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
+        quesitonCount = 0
+        playerScore = 0
     }
 }
 
