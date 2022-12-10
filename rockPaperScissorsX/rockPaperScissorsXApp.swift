ca7a54a4dee61a51c78a1e11aeba131512e66abd
thaddeus0605 <tdronski@gmail.com>
2022-12-09 19:21:13 -0600

    Initial Commit

---
+++ rockPaperScissorsX/rockPaperScissorsXApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorsXApp.swift
+//  rockPaperScissorsX
+//
+//  Created by Thaddeus Dronski on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct rockPaperScissorsXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
